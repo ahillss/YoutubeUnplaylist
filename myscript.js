f9a0f0e651c432737c3f5ac6a2d57d8ccc2e6de7
andrewhills <andrewhills@users.noreply.github.com>
2022-01-31 07:04:23 +1100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,11 +1,11 @@
 function unplaylist() {
     document.querySelector('[aria-label="Save to playlist"]').click();
 
-    var checkExist1 = setInterval(() => {
+    var checkExist = setInterval(() => {
         var m = document.querySelector('.ytd-add-to-playlist-renderer [aria-label="Cancel"]');
         
         if (m) {
-            clearInterval(checkExist1);
+            clearInterval(checkExist);
                     
             setTimeout(() => {
                 document.querySelector('.ytd-add-to-playlist-renderer [aria-label="Cancel"]').click();
@@ -18,20 +18,33 @@ function unplaylist() {
     }, 100);
 }
 
-var checkExist2 = setInterval(() => {
-    var m = document.querySelector("#menu-container");
-    
-    if (m) {
-        var element = document.createElement("button");
-        
-        element.innerHTML = "UNPLAYLIST";
-        element.style.backgroundColor = "transparent";
-        element.style.border = "none";
-        element.onclick = unplaylist;
-
-        //m.querySelector("#top-level-buttons-computed").appendChild(element);
-        m.querySelector("#menu").childNodes[0].appendChild(element);
-        
-        clearInterval(checkExist2);
+function go() {
+    if(!location.pathname.startsWith('/watch')) {
+        return;
     }
-}, 1000);
+    
+    var checkExist = setInterval(() => {
+        var m = document.querySelector("#menu-container");
+        
+        if (m) {
+            if(!document.getElementById("UNPLAYLIST")) {
+                var element = document.createElement("button");
+                
+                element.id = "UNPLAYLIST";
+                element.innerHTML = "UNPLAYLIST";
+                element.style.backgroundColor = "transparent";
+                element.style.border = "none";
+                element.onclick = unplaylist;
+
+                //m.querySelector("#top-level-buttons-computed").appendChild(element);
+                m.querySelector("#menu").childNodes[0].appendChild(element);
+            }
+            
+            clearInterval(checkExist);
+        }
+    }, 1000);
+}
+
+document.addEventListener('yt-navigate-finish', go);
+
+go();
